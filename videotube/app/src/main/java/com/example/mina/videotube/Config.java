1cd6dafaf22074104432148e14a0468fb3d1bb7e
MinaFouadYouesf <m.f.youesf2016@gmail.com>
2018-08-27 13:41:14 +0200

    youtube APi

---
+++ videotube/app/src/main/java/com/example/mina/videotube/Config.java
@@ -0,0 +1,10 @@
+package com.example.mina.videotube;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBdYd1K_zNmBH-CDCJ0sq0cgWtpfvl_03o";
+
+}
